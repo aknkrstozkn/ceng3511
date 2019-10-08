04c7a09bae5647407b0ced9c63b077c5a637ff92
root <root@DESKTOP-TSJVHTI.localdomain>
2019-10-09 02:03:44 +0300

    final commit

--- p1/search.py
+++ p1/search.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python3
 
 #Regex and System Packages
-import sys,re
+import sys,re,operator,collections
 #Package For Better Dictionary Parsing
 from ast import literal_eval
 
@@ -49,6 +49,8 @@ startKey = str(input("Please enter the start state : "))
 goalKey = str(input("Please enter the goal state : "))
 #######----Getting Start and Goal Keys As Console Inputs----#######
 
+#############################################------Searching Algorithms------#############################################
+
 #######----Breadth-First Search----#######
 def BreadthFirstSearch(dic, goalKey, startKey):
     #Creating List For Visited Values
@@ -79,4 +81,84 @@ def BreadthFirstSearch(dic, goalKey, startKey):
 BreadthFirstSearch(dic, goalKey, startKey)
 #######----Breadth-First Search----#######
 
+#######----Depth-First Search----#######
+def DepthFirstSearch(dic, goalKey, startKey):
+    #Creating List For Visited Values
+    visited = []
+    #Adding Start Key Beginning Of The Script Because We Always Visit It
+    visited.append(startKey)
+    #Creating Stack For DFS usage(Its Techniclay Not A Stack But We Use It Like Stack)
+    stack = []
+    #Creating A List To Hold Paths(Also This List Works As Stack)
+    stringList = []
+    
+    ###--Recursive Implementation Of Recursive Depth-First Search--###
+    def DFS(string, key):
+        returnKey = None
+        
+        #If We Hit Goal Then Return Path
+        if key == goalKey:
+            return string;
+        #If We Dont Hit Yet Than Appending Not Visited Keys And Their Paths
+        else:        
+            for item in dic[key]:
+                if item not in visited:
+                    stack.append(item)
+                    visited.append(item)
+                    stringList.append(string + " - " + item)
+                    returnKey = item
+                    break
+            if returnKey is None:
+                stringList.pop()
+                stack.pop()
+                return DFS(stringList[-1], stack[-1])
+            
+            return DFS(stringList[-1], stack[-1])            
+    ###--Recursive Implementation Of Recursive Depth-First Search--###
+    print("DFS : " + DFS(startKey,startKey))
 
+DepthFirstSearch(dic, goalKey, startKey)
+#######----Depth-First Search----#######
+
+#######----Uniform-Cost Search----#######
+def UniformCostSearch(dic, goalKey, startKey):    
+    #Creating List For Visited Values
+    visited = []
+    #Adding Start Key Beginning Of The Script Because We Always Visit It
+    visited.append(startKey)
+    #Creating Queue For UCS usage(Its Techniclay Not A Queue But We Use It Like Queue)
+    queue = []
+    #Creating A List To Hold Paths(Also This List Works As Queue)
+    stringList = []
+    
+    #Shortind Dictionaries By Value To Get Least Cost Path
+    def ShortingDictionary(dic):
+        newDic = {}
+        for dictionary in dic:
+            secondDic = {}
+            for key, value in sorted(dic[dictionary].items(), key=lambda kv: kv[1], reverse=False):
+                secondDic[key] = value
+            newDic[dictionary] = secondDic   
+        return newDic    
+    
+    dic= ShortingDictionary(dic)
+    ###--Recursive Implementation Of Recursive Uniform-Cost Search--###
+    def UCS(string, key):
+        #If We Hit Goal Then Return Path
+        if key == goalKey:
+            return string;
+        #If We Dont Hit Yet Than Appending Not Visited Keys And Their Paths
+        else:            
+            for item in dic[key]:
+                if item not in visited:
+                    queue.append(item)
+                    visited.append(item)
+                    stringList.append(string + " - " + item)
+            return UCS(stringList.pop(0), queue.pop(0))            
+    ###--Recursive Implementation Of Recursive Uniform-Cost Search--###
+    print("UCS : " + UCS(startKey,startKey))
+
+
+
+UniformCostSearch(dic, goalKey, startKey)
+#######----Uniform-Cost Search----#######
